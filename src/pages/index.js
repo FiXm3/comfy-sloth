f06d527a6cf899bc917c89d3cd08a1f0c789892c
Marek Ziolko <marekziolko3@gmail.com>
2021-08-09 18:06:07 +0100

    project files

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import Error from './ErrorPage';
+import About from './AboutPage';
+import SingleProduct from './SingleProductPage';
+import Cart from './CartPage';
+import Checkout from './CheckoutPage';
+import PrivateRoute from './PrivateRoute';
+
+export {
+  Home,
+  Products,
+  Error,
+  About,
+  SingleProduct,
+  Cart,
+  Checkout,
+  PrivateRoute,
+};
---
+++ src/utils/helpers.js
@@ -0,0 +1,8 @@
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat('en-UK', {
+    style: 'currency',
+    currency: 'GBP',
+  }).format(number / 100);
+};
+
+export const getUniqueValues = () => {};
